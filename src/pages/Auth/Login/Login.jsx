07053cb16c0682c1bcd4fcfd27f4a362cc12471a
AK Zaber <zaberkhan611@gmail.com>
2026-04-19 19:47:57 +0600

    register alongside login

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+import { Link } from 'react-router';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const {signInUser} = useAuth();
+    const handleLogIn = (data) => {
+        console.log('login data', data)
+        signInUser(data.email, data.password)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
     return (
-        <form action="">
-            <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
-                <input type="password" className="input" placeholder="Password" />
-                <div><a className="link link-hover">Forgot password?</a></div>
-                <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
-        </form>
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl p-6">
+            <h3 className="text-3xl font-extrabold text-center">Welcome Back</h3>
+            <p className='text-center'>Please Login</p>
+            <form onSubmit={handleSubmit(handleLogIn)} className="card-body">
+                <fieldset className="fieldset">
+                    {/* Email field */}
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email', { required: true })} className="input" placeholder="Email" />
+                    {errors.email?.type === "required" && <p className='text-red-500'>Email is required.</p>}
+                    {/* Password field */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register("password", { required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/ })} className="input" placeholder="Password" />
+                    {errors.password?.type === "required" && <p className='text-red-500'>Password is required.</p>}
+                    {errors.password?.type === "minLength" && <p className='text-red-500'>Password must be 6 characters or more</p>}
+                    {errors.password?.type === "pattern" && <p className='text-red-500'>Password must have one uppercase, one lowercase, one number and one special character</p>}
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+                <p>New to ProFast <Link className='text-secondary font-bold underline' to='/register'>Register Now</Link></p>
+            </form>
+        </div>
     );
 };
 
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+import { Link } from 'react-router';
 
 const Register = () => {
-    const {register, handleSubmit, formState:{errors}} = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { registerUser } = useAuth();
     const handleRegistration = (data) => {
         console.log(data);
+        registerUser(data.email, data.password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
     return (
-        <form onSubmit={handleSubmit(handleRegistration)}>
-            <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" {...register("email", {required:true})} className="input" placeholder="Email" />
-                {errors.email?.type==="required" && <p className='text-red-500'>Email is required.</p>}
-                {/* password */}
-                <label className="label">Password</label>
-                <input type="password" {...register("password", {required:true, minLength:6})} className="input" placeholder="Password" />
-                {errors.password?.type==="required" && <p className='text-red-500'>Password is required.</p>}
-                {errors.password?.type==="minLength" && <p className='text-red-500'>Password must be 6 characters or more</p>}
-                <div><a className="link link-hover">Forgot password?</a></div>
-                <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
-        </form>
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl p-6">
+            <h3 className="text-3xl font-extrabold text-center">Welcome to ProFast</h3>
+            <p className='text-center'>Please Create an Account</p>
+            <form className="card-body" onSubmit={handleSubmit(handleRegistration)}>
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" {...register("email", { required: true })} className="input" placeholder="Email" />
+                    {errors.email?.type === "required" && <p className='text-red-500'>Email is required.</p>}
+                    {/* password */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register("password", { required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/ })} className="input" placeholder="Password" />
+                    {errors.password?.type === "required" && <p className='text-red-500'>Password is required.</p>}
+                    {errors.password?.type === "minLength" && <p className='text-red-500'>Password must be 6 characters or more</p>}
+                    {errors.password?.type === "pattern" && <p className='text-red-500'>Password must have one uppercase, one lowercase, one number and one special character</p>}
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Register</button>
+                </fieldset>
+                <p>Already Have an Account <Link to="/login" className='text-secondary font-bold underline'>Login</Link></p>
+            </form>
+        </div>
     );
 };
 
