b8a111620df92edf9b913f0116658a7cdc4cba8e
AK Zaber <zaberkhan611@gmail.com>
2026-06-19 23:08:03 +0600

    store usesInfo in the database

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -4,42 +4,54 @@ import useAuth from '../../../hooks/useAuth';
 import { Link, useLocation, useNavigate } from 'react-router';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import axios from 'axios';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const Register = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const { registerUser, updateUserProfile } = useAuth();
     const location = useLocation();
     const navigate = useNavigate();
-    console.log(location);
+    const axiosSecure = useAxiosSecure();
 
     const handleRegistration = (data) => {
-        console.log(data);
         const profileImg = data.photo[0];
         registerUser(data.email, data.password)
-            .then(result => {
-                console.log(result.user);
+            .then(() => {
                 // store the image and get the photo url
                 const formData = new FormData();
                 formData.append('image', profileImg);
                 const image_API_URL = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host_key}`;
 
                 axios.post(image_API_URL, formData)
-                .then(res => {
-                    console.log('after upload the image', res);
-                    //Update user profile here
-                    const userProfile = {
-                        displayName: data.name,
-                        photoURL:res.data.data.url
-                    }
-                    updateUserProfile(userProfile)
-                    .then(() => {
-                        console.log('user profile updated done')
-                        navigate(location?.state || '/');
+                    .then(res => {
+                        const photoURL = res.data.data.url;
+
+                        // Create user in the database
+                        const userInfo = {
+                            email: data.email,
+                            displayName: data.name,
+                            photoURL: photoURL
+                        }
+                        axiosSecure.post('/users', userInfo)
+                            .then(res => {
+                                if (res.data.insertedId) {
+                                    console.log('user created in the database');
+                                }
+                            })
+
+                        //Update user profile here
+                        const userProfile = {
+                            displayName: data.name,
+                            photoURL: photoURL
+                        }
+                        updateUserProfile(userProfile)
+                            .then(() => {
+                                navigate(location?.state || '/');
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            })
                     })
-                    .catch(error => {
-                        console.log(error);
-                    })
-                })
             })
             .catch(error => {
                 console.log(error);
--- src/pages/Auth/SocialLogin/SocialLogin.jsx
+++ src/pages/Auth/SocialLogin/SocialLogin.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import { useLocation, useNavigate } from 'react-router';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const SocialLogin = () => {
-    const {signInGoogle} = useAuth();
+    const { signInGoogle } = useAuth();
     const location = useLocation();
+    const axiosSecure = useAxiosSecure();
     const navigate = useNavigate();
 
     const handleGoogleSignIn = () => {
         signInGoogle()
-        .then(result => {
-            console.log(result.user);
-            navigate(location?.state || '/');
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(result => {
+                console.log(result.user);
+
+                // Create user in the database
+                const userInfo = {
+                    email: result.user.email,
+                    displayName: result.user.displayName,
+                    photoURL: result.user.photoURL
+                }
+                axiosSecure.post('users', userInfo)
+                    .then(res => {
+                        console.log('user data has been stored', res.data);
+                        navigate(location?.state || '/');
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
     return (
         <div className='text-center space-y-4'>
